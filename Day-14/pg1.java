9d00d540385d8675a339debe6084d2a30d6cd57f
NAVEEN RAJ <naveenraj.dayalan07@gmail.com>
2025-08-07 20:40:46 +0530

    Update pg1.java
    Update pg1.java

--- Day-14/pg1.java
+++ Day-14/pg1.java
@@ -3,10 +3,10 @@ public class pg1
 {
 	public static void main(String[] args) {
 		List <String> list=new ArrayList<>();
-		list.add("Varshni");
-		list.add("Dharshni");
-		list.add("Vishnu");
-		list.add("Sanjay");
+		list.add("tom");
+		list.add("clint");
+		list.add("jonas");
+		list.add("max");
 		
 		for(String i:list){
 		    System.out.println(i);
