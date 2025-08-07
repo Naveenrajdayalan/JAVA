91d016e8bd850bdbfd5c24460d52f64350e6d908
NAVEEN RAJ <naveenraj.dayalan07@gmail.com>
2025-08-07 20:39:48 +0530

    Add files via upload

---
+++ Day-14/pg1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class pg1
+{
+	public static void main(String[] args) {
+		List <String> list=new ArrayList<>();
+		list.add("Varshni");
+		list.add("Dharshni");
+		list.add("Vishnu");
+		list.add("Sanjay");
+		
+		for(String i:list){
+		    System.out.println(i);
+		}
+	}
+}
